0f32797cdc1479b43f3d97457420c9875ea8495d
Aman Sharma <aman11.chess@gmail.com>
2026-07-15 03:21:44 -0400

    edited trancribe endpoint

--- backend/audio_to_speech/test/test_audio_to_speech.py
+++ backend/audio_to_speech/test/test_audio_to_speech.py
@@ -17,7 +17,6 @@ def test_transcription(audio_file_path):
     print("----------------------------")
 
 if __name__ == "__main__":
-
     # Your specific path
     audio_file = r"C:\Users\chess\Desktop\misc\my_projects\notetaker\backend\audio_to_speech\test\test_audio_22_sec.mp3"
     
